b3a48e413e46461e3a5f5a48c9ae986776c677ee
ARYAN NIGAM <aryannigam18@gmail.com>
2021-04-13 02:18:50 +0530

    authentication api created

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const ConnectDB = require('./config/db')
+
 //Connect DB
 ConnectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
+
+//Routes
 app.get('/', (req, res) => res.send("Hello"));
+app.use('/api/admin', require('./routes/api/adminUser'));
+app.use('/api/auth', require('./routes/api/auth'));
+
 //port init
 const port = process.env.port || 5000;
 app.listen(port, () => console.log(`Api is running on ${port}`));
