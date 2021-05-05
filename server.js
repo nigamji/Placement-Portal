4484268e05ec369a8c829e7b204a7eea82b8e27b
ANUJ YADAV <rockanuj1999@gmail.com>
2021-05-05 22:51:18 +0530

    aded Pagination

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/drive', require('./routes/api/drives'));
 app.use('/api/student', require('./routes/api/student'));
 app.use('/api/filter/', require('./routes/api/filter'));
+app.use('/api/page', require('./routes/api/page'));
 //port init
 const port = process.env.port || 5000;
 app.listen(port, () => console.log(`Api is running on ${port}`));
